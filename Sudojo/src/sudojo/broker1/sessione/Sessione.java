377037bce014a59aa47527e0513ee07678013555
Eimitfi <macarena7799@gmail.com>
2021-06-29 17:31:56 +0200

    aggiunta lavoro preparatorio di Simone al repo

---
+++ Sudojo/src/sudojo/broker1/sessione/Sessione.java
@@ -0,0 +1,25 @@
+package sudojo.broker1.sessione;
+
+import java.util.Date;
+
+public class Sessione {
+private String utente;
+private Date dataInizio;
+private Date dataScadenza;
+
+public Ruolo getRuolo() {
+	
+}
+
+public Persona getPersona() {
+	
+}
+
+public Date getDataInizio() {
+	return this.dataInizio;
+}
+
+public Date getDataScadenza() {
+	return this.dataScadenza;
+}
+}
