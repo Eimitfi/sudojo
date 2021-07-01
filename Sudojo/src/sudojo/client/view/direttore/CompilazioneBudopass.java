9d9aaef0de9cfa014ee2c47fc2317b39ed773185
IlBaddynatore <48033778+IlBaddynatore@users.noreply.github.com>
2021-07-01 18:24:43 +0200

    Dubbio su due maschere [LEGGERE DESCRIZIONE]
    Su queste due maschere ho un dubbio e non saprei se agganciarle al viewmodel o meno. Mi pare di no, ma magari ne discutiamo. Ho comunque lasciato commenti abbastanza autoesplicativi

--- Sudojo/src/sudojo/client/view/direttore/CompilazioneBudopass.java
+++ Sudojo/src/sudojo/client/view/direttore/CompilazioneBudopass.java
@@ -4,7 +4,9 @@ import sudojo.client.view.AbstractView;
 import sudojo.client.viewmodel.Subject;
 
 public class CompilazioneBudopass extends AbstractView{
-	
+	//Se non ricordo male (non ho trovato nulla nei documenti di progetto) questa è la classe che effettivamente si occupa di compilare
+	//il budopass e quindi di aggiornare il DB, mentre MascheraCompilaBudopass è la maschera in cui si inseriscono i dati.
+	//C'è da parlarne un attimo per chiarire
 	
 	@Override
 	public void onChange(Subject obj) {
--- Sudojo/src/sudojo/client/view/direttore/ConfermaBudopass.java
+++ Sudojo/src/sudojo/client/view/direttore/ConfermaBudopass.java
@@ -4,6 +4,9 @@ import sudojo.client.view.AbstractView;
 import sudojo.client.viewmodel.Subject;
 
 public class ConfermaBudopass extends AbstractView{
+	//Questa dovrebbe essere solo una maschera che appare per chiedere conferma dei dati inseriti e poi porta ad un'altra maschera
+	//(probabilmente quella principale), quindi non deve essere agganciata al viewmodel. Sempre se non mi sono sbagliato
+	
 	@Override
 	public void onChange(Subject obj) {
 		// TODO Auto-generated method stub	
