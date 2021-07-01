8ecacb0fb2f73ec0a2852bdf6bdcdf4d17ab77d5
IlBaddynatore <48033778+IlBaddynatore@users.noreply.github.com>
2021-07-01 18:31:30 +0200

    Ulteriore dubbio
    Anche qui, commento autoesplicativo

--- Sudojo/src/sudojo/client/view/gestore/AggiuntaGenitori.java
+++ Sudojo/src/sudojo/client/view/gestore/AggiuntaGenitori.java
@@ -4,7 +4,10 @@ import sudojo.client.view.AbstractView;
 import sudojo.client.viewmodel.Subject;
 
 public class AggiuntaGenitori extends AbstractView{
-	
+	//Dubbio su questa classe, perché l'aggiunta/modifica dei genitori viene fatta attraverso CreaAffiliatoViewModelInterface e
+	//ModificaAffiliatoViewModelInterface, quindi non so se bisognerebbe agganciarla al viewmodel oppure no. Nel caso non saprei
+	//nemmeno come farlo in grafica perché appunto dovrebbe già essere parte di aggiunta/modifica affiliato ed essendo una classe
+	//della view non dovrei implementare logica, a regola. Ditemi voi
 	
 	@Override
 	public void onChange(Subject obj) {
