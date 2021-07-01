e69e83b6a0f84e388788717a4a252b6272901703
telespalladaniel <73735603+telespalladaniel@users.noreply.github.com>
2021-07-01 17:06:04 +0200

    leggere il commento in Subject
    dovrei aver praticamente finito il viewmodel, mi metto a ricontrollare cose qua e là e in caso vado un po avanti con il broker. nel commento in Subject è spiegato bene il flusso di lavoro tra view viewmodel e model (esito delle modifiche fatte oggi, per come avevamo progettato le robe sta parte di observer+http l'avevamo totalmente scazzata)

--- Sudojo/src/sudojo/client/viewmodel/Subject.java
+++ Sudojo/src/sudojo/client/viewmodel/Subject.java
@@ -9,3 +9,9 @@ public interface Subject {
 	public ResponseInterface getResponse();
 
 }
+//flow: la maschera (che è un observer) si binda tramite attach alla classe del viewmodel (che è un subject) che gli serve e poi 
+//ne invoca i metodi. tali metodi sono void e asincroni. la rispsota arriva al viewmodel tramite
+//httpsclient e quando arriva tale risposta il viewmodel aggiorna tutti i suoi osservatori
+//(tramite notifyObserver che per ogni observer ne fa observer.onChange(this))
+//nella onChange(Subject subject) la maschera fa subject.getResponse() per ottenere
+//la risposta che arriva dal broker e maneggiarla opportunamente
