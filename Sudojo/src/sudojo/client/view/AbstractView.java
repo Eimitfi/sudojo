020d3412096610ec0f9dd4b39798d5372c989e24
telespalladaniel <73735603+telespalladaniel@users.noreply.github.com>
2021-07-01 20:31:48 +0200

    dubbio su abstractView
    secondo me meglio lasciarla astratta perché in abstractViewModel ho avuto necessità di fattorizzare metodi ricorrenti in tutte le viewmodel

--- Sudojo/src/sudojo/client/view/AbstractView.java
+++ Sudojo/src/sudojo/client/view/AbstractView.java
@@ -7,6 +7,7 @@ public abstract class AbstractView implements Observer<Subject>{
 	//Dubbio: draw e bind dovrebbero essere astratte perche ogni oggetto le implementa a modo suo. Messa cosi la classe sembra molto
 	//un'interfaccia, dobbiamo lasciarla astratta lo stesso per dare una gerarchia ereditaria o e meglio trasformarla in interfaccia?
 	//Questo anche perche non credo ci siano altri metodi da metterci dentro
+	//[daniel]: vero x draw e bind, x il resto secondo me meglio lasciarla astratta perché potremmo accorgerci che ci sono operazioni comuni alle maschere da fattorizzare (in AbstractViewModel mi è successo)
 	
 	public abstract void draw();
 	public abstract void bind();
