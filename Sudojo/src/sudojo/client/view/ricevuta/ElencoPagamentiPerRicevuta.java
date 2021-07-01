dc63b5191b903a8d3fb6fe7b85cdc8caff4acb6d
IlBaddynatore <48033778+IlBaddynatore@users.noreply.github.com>
2021-07-01 19:20:31 +0200

    Ultimo dubbio di oggi

--- Sudojo/src/sudojo/client/view/ricevuta/ElencoPagamentiPerRicevuta.java
+++ Sudojo/src/sudojo/client/view/ricevuta/ElencoPagamentiPerRicevuta.java
@@ -5,6 +5,8 @@ import sudojo.client.viewmodel.Subject;
 import sudojo.client.viewmodel.ricevute.RicevuteViewModelInterface;
 
 public class ElencoPagamentiPerRicevuta extends AbstractView{
+	//Dubbio fast: e giusto collegare questa classe a RicevuteViewModelInterface o bisognerebbe collegarla a GetPagamentiViewModelInterface?
+	
 	private RicevuteViewModelInterface viewmodel;
 	
 	@Override
