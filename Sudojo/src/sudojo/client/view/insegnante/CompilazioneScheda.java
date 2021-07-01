0248e82a81e2483156e341c135abc5754afefba4
IlBaddynatore <48033778+IlBaddynatore@users.noreply.github.com>
2021-07-01 18:44:04 +0200

    Ulteriore dubbio uguale a quello su CompilazioneBudopass
    Commenti autoesplicativi for the win

--- Sudojo/src/sudojo/client/view/insegnante/CompilazioneScheda.java
+++ Sudojo/src/sudojo/client/view/insegnante/CompilazioneScheda.java
@@ -4,14 +4,11 @@ import sudojo.client.view.AbstractView;
 import sudojo.client.viewmodel.Subject;
 
 public class CompilazioneScheda extends AbstractView{
-
-
-
-
+	//Come per CompilazioneBudopass (riferirsi al commento che ho scritto lì), questa non dovrebbe essere una maschera ma la classe
+	//che si occupa effettivamente di compilare la scheda e aggiornare il DB. Quindi il dubbio rimane lo stesso
+	
 	@Override
 	public void onChange(Subject obj) {
 		// TODO Auto-generated method stub
-				
 	}
-
 }
