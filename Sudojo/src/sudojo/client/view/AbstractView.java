4c3aa198d1c60ef6f693ce6ccd39a1324af10b5e
IlBaddynatore <48033778+IlBaddynatore@users.noreply.github.com>
2021-07-01 18:50:58 +0200

    Dubbio su AbstractView

--- Sudojo/src/sudojo/client/view/AbstractView.java
+++ Sudojo/src/sudojo/client/view/AbstractView.java
@@ -4,11 +4,10 @@ import sudojo.client.viewmodel.Observer;
 import sudojo.client.viewmodel.Subject;
 
 public abstract class AbstractView implements Observer<Subject>{
-	public void draw() {
-		
-	}
+	//Dubbio: draw e bind dovrebbero essere astratte perche ogni oggetto le implementa a modo suo. Messa cosi la classe sembra molto
+	//un'interfaccia, dobbiamo lasciarla astratta lo stesso per dare una gerarchia ereditaria o e meglio trasformarla in interfaccia?
+	//Questo anche perche non credo ci siano altri metodi da metterci dentro
 	
-	public void bind() {
-		
-	}
+	public abstract void draw();
+	public abstract void bind();
 }
--- Sudojo/src/sudojo/client/view/iscritto/MascheraBudopass.java
+++ Sudojo/src/sudojo/client/view/iscritto/MascheraBudopass.java
@@ -2,16 +2,13 @@
 
 import sudojo.client.view.AbstractView;
 import sudojo.client.viewmodel.Subject;
+import sudojo.client.viewmodel.budopass.GetBudopassViewModelInterface;
 
 public class MascheraBudopass extends AbstractView{
-
-
-
-
+	private GetBudopassViewModelInterface viewmodel;
+	
 	@Override
 	public void onChange(Subject obj) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
-
 }
